8007d35ce927d0f7547477bb4a5136652fb3e836
Jonathan <jonathan.clus@holbertonschool.com>
2023-05-11 06:43:26 +0000

    Update setup script

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y make curl
+sudo apt-get update
 
 # Download Hugo binary
 HUGO_VERSION="0.84.0"
 HUGO_BINARY="hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
 curl -LO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}"
-          https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 dpkg -i ${HUGO_BINARY}
 rm ${HUGO_BINARY}
 
