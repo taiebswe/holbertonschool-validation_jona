596ed81ff095979659a60c63206fe03df6796ef0
Jonathan <jonathan.clus@holbertonschool.com>
2023-05-11 06:45:17 +0000

    Update setup script

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -6,13 +6,13 @@ sudo apt-get update
 HUGO_VERSION="0.84.0"
 HUGO_BINARY="hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
 curl -LO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}"
-dpkg -i ${HUGO_BINARY}
+sudo dpkg -i ${HUGO_BINARY}
 rm ${HUGO_BINARY}
 
 GOLANGCILINT_VERSION="1.36.0"
 curl --silent --show-error --location --output /tmp/golangci-lint.deb \
      "https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCILINT_VERSION}/golangci-lint-${GOLANGCILINT_VERSION}-linux-amd64.deb"
-dpkg -i /tmp/golangci-lint.deb
+sudo dpkg -i /tmp/golangci-lint.deb
 rm -f /tmp/golangci-lint.deb
 
 exit $?
