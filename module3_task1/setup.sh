4edeb4d237b8e3403f476d0c3a373eab24771dcb
Jonathan <jonathan.clus@holbertonschool.com>
2023-05-11 06:14:55 +0000

    module3)task1

---
+++ module3_task1/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y make curl
+
+# Download Hugo binary
+HUGO_VERSION="0.84.0"
+HUGO_BINARY="hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
+curl -LO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}"
+          https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
+dpkg -i ${HUGO_BINARY}
+rm ${HUGO_BINARY}
+
+exit $?
