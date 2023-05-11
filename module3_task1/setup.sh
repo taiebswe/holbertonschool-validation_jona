2eaf8a4e1714e23e861b936ab2e84aa293705a76
Lestt <jonathan.clus@holbertonschool.com>
2023-05-11 18:05:03 +1000

    Test

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y make curl build-essential
+sudo apt-get update && sudo apt-get install -y make curl build-essential
 
 GO_VERSION="1.15.7"
 CGO_ENABLED=1
@@ -16,13 +16,13 @@ cp /usr/local/go/bin/go /usr/local/bin
 HUGO_VERSION="0.84.0"
 HUGO_BINARY="hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
 curl -LO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}"
-dpkg -i ${HUGO_BINARY}
+sudo dpkg -i ${HUGO_BINARY}
 rm ${HUGO_BINARY}
 
 GOLANGCILINT_VERSION="1.52.2"
 curl --silent --show-error --location --output /tmp/golangci-lint.deb \
      "https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCILINT_VERSION}/golangci-lint-${GOLANGCILINT_VERSION}-linux-amd64.deb"
-dpkg -i /tmp/golangci-lint.deb
+sudo dpkg -i /tmp/golangci-lint.deb
 rm -f /tmp/golangci-lint.deb
 
 exit $?
